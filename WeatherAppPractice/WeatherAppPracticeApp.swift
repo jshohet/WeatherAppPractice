215be6f246eff67e2f3f3253caf54047dedd6965
Josef Shohet <joe.shohet@gmail.com>
2024-10-27 11:29:12 -0400

    Initial Commit

---
+++ WeatherAppPractice/WeatherAppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppPracticeApp.swift
+//  WeatherAppPractice
+//
+//  Created by Josef Shohet on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppPracticeTests/WeatherAppPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppPracticeTests.swift
+//  WeatherAppPracticeTests
+//
+//  Created by Josef Shohet on 10/27/24.
+//
+
+import Testing
+@testable import WeatherAppPractice
+
+struct WeatherAppPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
